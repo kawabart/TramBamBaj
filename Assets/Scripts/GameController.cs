cd2892b6ddeba8f70db57d3fb7fa6b6e961d3d62
Bartłomiej Kawa <kawaxbart@gmail.com>
2025-02-15 23:59:43 +0100

    chore: Initial commit
    Upload version from 0h Game Jam (2024) w Tramwaju

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private SpriteRenderer photoHolder;
+    [SerializeField] private Sprite[] photos;
+    [SerializeField] private float[] values;
+    float currentValue = 0.5f;
+    private Sprite photo;
+    int state = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //photo = GetComponent<Sprite>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float change = 0.001f + Time.timeSinceLevelLoad / 10000;
+        float input = Input.GetAxis("Horizontal");
+        Debug.Log(input);
+
+        if (input < 0)
+        {
+            currentValue -= 0.01f;
+        }
+        else if (input > 0)
+        {
+            currentValue += 0.01f;
+        }
+
+        if (currentValue < 0.5f)
+        {
+            currentValue -= change;
+        }
+        else if (currentValue > 0.5f)
+        {
+            currentValue += change;
+        }
+
+        slider.value = currentValue;
+
+        //for (int i = 0; i < photos.Length; i++) { }
+
+        int index = 0;
+
+        foreach (float val in values)
+        {
+            if (currentValue <= val)
+            {
+                photoHolder.sprite = photos[index];
+                if (currentValue > 0.5f)
+                {
+                    photoHolder.flipX = true;
+                }
+                else
+                {
+                    photoHolder.flipX = false;
+                }
+                break;
+            }
+            index++;
+        }
+
+    }
+}
