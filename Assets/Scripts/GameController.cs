a8a3f6996e9be79902df38a06bf643fe904c637f
Bartłomiej Kawa <kawaxbart@gmail.com>
2025-02-23 23:59:48 +0100

    refactor: Remove unused and simplify existing code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,65 +6,35 @@ public class GameController : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private SpriteRenderer photoHolder;
     [SerializeField] private Sprite[] photos;
-    [SerializeField] private float[] values;
+    [SerializeField] private float[] thresholds;
     float currentValue = 0.5f;
-    private Sprite photo;
-    int state = 2;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //photo = GetComponent<Sprite>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         float change = 0.001f + Time.timeSinceLevelLoad / 10000;
         float input = Input.GetAxis("Horizontal") * Time.deltaTime;
-        Debug.Log(input);
 
-        if (input < 0)
-        {
-            currentValue += input;
-        }
-        else if (input > 0)
-        {
-            currentValue += input;
-        }
+        currentValue += input;
 
         if (currentValue < 0.5f)
-        {
             currentValue -= change;
-        }
         else if (currentValue > 0.5f)
-        {
             currentValue += change;
-        }
 
         slider.value = currentValue;
 
-        //for (int i = 0; i < photos.Length; i++) { }
+        Debug.Log(currentValue);
 
-        int index = 0;
-
-        foreach (float val in values)
+        for (int i = 0; i < thresholds.Length; i++)
         {
-            if (currentValue <= val)
+            if (currentValue <= thresholds[i])
             {
-                photoHolder.sprite = photos[index];
-                if (currentValue > 0.5f)
-                {
-                    photoHolder.flipX = true;
-                }
-                else
-                {
-                    photoHolder.flipX = false;
-                }
+                photoHolder.sprite = photos[i];
+                photoHolder.flipX = (currentValue > 0.5f);
+
                 break;
             }
-            index++;
         }
-
     }
 }
